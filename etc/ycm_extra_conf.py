0556d54dedf8815efb5ed77fc3d4ba306ef52554
liyong <liyong19861014@gmail.com>
2014-12-29 09:58:02 +0800

    modified:   ycm_extra_conf.py mac os c++ include path

--- etc/ycm_extra_conf.py
+++ etc/ycm_extra_conf.py
@@ -97,9 +97,9 @@ flags = [
 '-isystem',
 '/usr/local/include',
 '-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/c++/v1',
+'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
 '-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include'
 ]
 
 
